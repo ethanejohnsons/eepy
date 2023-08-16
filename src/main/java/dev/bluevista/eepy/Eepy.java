2179dc8e1902326898617b2b76ed17a987e8d3c8
ethanejohnsons <ethan@ejohnsons.com>
2023-08-16 11:10:16 -0500

    Third commit

--- src/main/java/dev/bluevista/eepy/Eepy.java
+++ src/main/java/dev/bluevista/eepy/Eepy.java
@@ -2,8 +2,10 @@ package dev.bluevista.eepy;
 
 import io.papermc.paper.event.player.PlayerDeepSleepEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerBedEnterEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Eepy extends JavaPlugin implements Listener {
@@ -11,11 +13,26 @@ public class Eepy extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
+        Bukkit.getLogger().info("I'm eepy.");
+    }
+
+    @EventHandler
+    public void onPlayerBedEnterEvent(PlayerBedEnterEvent event) {
+        String name = event.getPlayer().getName();
+        World world = event.getPlayer().getWorld();
+
+        if (world.getTime() >= 13_000 || (world.getTime() < 13_000 && world.isThundering())) {
+            world.getPlayers().forEach(player -> {
+                player.sendMessage(String.format("%s is eeping through the night.", name));
+            });
+        }
     }
 
     @EventHandler
     public void onPlayerDeepSleepEvent(PlayerDeepSleepEvent event) {
-        event.getPlayer().getWorld().setTime(0);
+        World world = event.getPlayer().getWorld();
+        world.setTime(0); // day time
+        world.setThundering(false); // no more weather
     }
 
 }
